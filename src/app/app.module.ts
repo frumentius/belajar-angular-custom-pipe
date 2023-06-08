7248c94bd4af70b9c203eed8412811eb9fb4664a
Frumentius <frumentius_sonic@yahoo.com>
2023-06-08 21:19:10 +0700

    custom pipe basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrefixPipe } from './custom-pipe-prefix';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrefixPipe
   ],
   imports: [
     BrowserModule
